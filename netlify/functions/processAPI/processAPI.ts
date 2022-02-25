89e789aaae16218ceb0d4560e39ba3d33048fd0b
cruyff97 <pelmat97@gmail.com>
2022-02-25 15:43:26 +0100

    fix netlify

--- netlify/functions/processAPI/processAPI.ts
+++ netlify/functions/processAPI/processAPI.ts
@@ -3,7 +3,7 @@ import { schedule } from '@netlify/functions';
 // To learn about scheduled functions and supported cron extensions, 
 // see: https://ntl.fyi/sched-func
 
-export function processEnv() {
+export async function processEnv() {
     return process.env['API_KEY_CT']
 }
 
